61e306d892952359c826e5e1ae70a0accf614ce9
Sangjun Lee <lsjmc0224@dgu.edu>
2024-09-14 01:34:30 +0900

    :sparkles: Add trailing slash setting into settings.pyh

--- gee/settings.py
+++ gee/settings.py
@@ -45,6 +45,10 @@ INSTALLED_APPS = [
     'main',
 ]
 
+REST_FRAMEWORK = {
+    'TRAILING_SLASH': False,
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
