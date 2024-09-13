3f6da930a613d31046ebdee3f7def281cb1324be
Sangjun Lee <lsjmc0224@dgu.edu>
2024-09-14 02:30:32 +0900

    :sparkles: Add music sound model and admin page customizing

--- main/serializers.py
+++ main/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import fields, serializers
 from .models import DAY_CHOICE
-from .models import Alarm
+from .models import Alarm, AlarmSound
 
 class AlarmSerializer(serializers.ModelSerializer):
     day = fields.MultipleChoiceField(choices=DAY_CHOICE)
+    sound = serializers.PrimaryKeyRelatedField(queryset=AlarmSound.objects.all(), required=True)
+
     class Meta:
         model = Alarm
-        fields = ['id', 'time', 'day', 'label']
+        fields = ['id', 'time', 'day', 'label','sound']
